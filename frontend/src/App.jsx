43b9408c22c8e631b906c4fd53d007262d7c96d7
Aiden31x <aryandadwal2004@gmail.com>
2025-05-20 19:48:57 +0530

    Working Paytm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-
+import {BrowserRouter, Routes,Route} from 'react-router-dom'
+import { SignUp } from './pages/SignUp'
+import { SignIn } from './pages/SignIn'
+import { SendMoney } from './pages/TransferMoney'
+import { Dashboard } from './pages/Dashboard'
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<SignIn />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/send' element={ <SendMoney />} />
+
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
